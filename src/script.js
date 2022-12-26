2dcb4bae9f837a6cd036a85495b84f8101818539
Mikko <64331305+mikko4@users.noreply.github.com>
2022-12-26 18:40:38 +0200

    update css + start implementing minimax

--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
-var board = null;
-var game = new Chess();
+let board = null;
+const game = new Chess();
 
 function onDragStart(source, piece, position, orientation) {
   // do not pick up pieces if the game is over
@@ -10,19 +10,47 @@ function onDragStart(source, piece, position, orientation) {
 }
 
 function makeRandomMove() {
-  var possibleMoves = game.moves();
+  const possibleMoves = game.moves();
 
   // game over
   if (possibleMoves.length === 0) return;
 
-  var randomIdx = Math.floor(Math.random() * possibleMoves.length);
+  const randomIdx = Math.floor(Math.random() * possibleMoves.length);
   game.move(possibleMoves[randomIdx]);
   board.position(game.fen());
 }
 
+function makeBestMove() {
+  if (game.moves().length === 0) {
+    alert("game over");
+    return;
+  }
+
+  bestMove = getBestMove(game);
+  game.move(bestMove);
+  board.position(game.fen());
+}
+
+const getBestMove = (game) => {
+  let positionCount = 0;
+  const depth = parseInt($("#search-depth").find(":selected").text());
+
+  const d1 = new Date().getTime();
+  const bestMove = minimax(depth, game);
+  const moveTime = new Date().getTime() - d1;
+
+  const positionsPerS = (positionCount * 1000) / moveTime;
+
+  $("#position-count").text(positionCount);
+  $("#time").text(moveTime / 1000 + "s");
+  $("#positions-per-s").text(positionsPerS);
+};
+
+const minimax = (depth, game) => {};
+
 function onDrop(source, target) {
   // see if the move is legal
-  var move = game.move({
+  const move = game.move({
     from: source,
     to: target,
     promotion: "q", // NOTE: always promote to a queen for example simplicity
@@ -41,7 +69,7 @@ function onSnapEnd() {
   board.position(game.fen());
 }
 
-var config = {
+const config = {
   draggable: true,
   position: "start",
   onDragStart: onDragStart,
